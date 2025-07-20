85ef438ff690c256f23ca2407a788ab4728fa304
shamilEllikkal <shamilellikkal123@gmail.com>
2025-07-21 01:19:55 +0530

    app.css issue resolved

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './app.css'
+import './App.css'
 import App from './App.jsx'
 import './fontawesome.js'
 
